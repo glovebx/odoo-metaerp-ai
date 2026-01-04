939c56fd980ada23500e427c6b238c6cd2a01146
glovebx <glovebx@glovebxdeMacBook-Pro-2.local>
2026-01-04 14:58:05 +0800

    add terms.html

--- proxy.js
+++ proxy.js
@@ -1,14 +1,15 @@
 import { locales } from './lib/i18n';
 import { NextRequest } from 'next/server';
 
+const exemptPathsSet = new Set(['/news.html', '/privacy.html', '/terms.html']);
 export function proxy(request) {
 	const { pathname } = request.nextUrl;
 
 	const isLocalizedPath = locales.some((locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`);
 
     // --- 定义不需要被重定向的特定路径 ---
-    // 将 这两个页面 添加到豁免列表
-    const isExemptPath = (pathname === '/news.html' || pathname === '/privacy.html');
+    // 将 页面 添加到豁免列表
+    const isExemptPath = exemptPathsSet.has(pathname);
 
     // 如果路径已经包含 locale，或者它是我们豁免的路径，则直接放行
     if (isLocalizedPath || isExemptPath) {
